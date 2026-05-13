776df8e636166737d67cc6d40f3953da4f784aa1
snr_cloud_eng <snr_cloud_eng@company.com>
2026-05-13 09:28:13 +0200

    modified App.js file

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React <br>
-          This is a new line I added.</br>
+          Learn React <br/>
+          This is a new line I added.
         </a>
       </header>
     </div>
